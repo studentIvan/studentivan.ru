d5733496fecc99f59209220cec4f8f945f5731e3
Student Ivan <cryptox3@gmail.com>
2014-01-30 03:24:20 +0400

    3:23 AM 30.01.2013

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,19 +1,25 @@
 function avatarIndexNext(currentIndex)
 {
-    var nextIndex = (currentIndex == 10) ? 1 : currentIndex + 1,
-    $image = $('section[role="avatar"] img');
+    var nextIndex = (currentIndex == 10) ? 3 : currentIndex + 1, $image1 = $('#avatar1');
+    $('<img id="avatar2" src="images/avatars/' + nextIndex + '.png">').insertAfter($image1);
+    var $image2 = $('#avatar2');
     setTimeout(function() 
     {
-        $image.fadeTo('slow', 0.01, function() {
-            $(this).attr('src', 'images/avatars/' + nextIndex + '.png');
-            $(this).fadeTo('slow', 1, function() {
-                avatarIndexNext(nextIndex);
-            });
+        $image1.fadeOut(2000, function() {
+            $image1.remove();
+            $image2.attr('id', 'avatar1');
+            avatarIndexNext(nextIndex);
         });
     }, 4000);
 }
 
 $(document).ready(function() {
     $('section[role="left-panel"]').css('height', $(window).height() + 'px');
-    avatarIndexNext(1);
+    avatarIndexNext(3);
+
+    /*$('section[role="left-panel"] a').hover(function() {
+        $(this).find('i').addClass('fa-spin');
+    }, function() {
+        $(this).find('i').removeClass('fa-spin');
+    });*/
 })
