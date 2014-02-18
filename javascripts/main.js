dd0649e112797e2b4ae673f240721c3c90e59f22
Student Ivan <cryptox3@gmail.com>
2014-02-18 20:53:56 +0400

    updt

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,26 +1,29 @@
-/*function avatarIndexNext(currentIndex)
-{
-    var nextIndex = (currentIndex == 10) ? 3 : currentIndex + 1, $image1 = $('#avatar1');
-    $('<img id="avatar2" src="images/avatars/' + nextIndex + '.png">').insertAfter($image1);
-    var $image2 = $('#avatar2');
-    setTimeout(function() 
-    {
-        $image1.fadeOut(2000, function() {
-            $image1.remove();
-            $image2.attr('id', 'avatar1');
-            avatarIndexNext(nextIndex);
-        });
-    }, 4000);
-}
-*/
-$(document).ready(function() {
-    $('section[role="left-panel"]').css('height', $(window).height() + 'px');
-    $('section[role="page"]').css('height', $(window).height() - 30 + 'px');
-    /*avatarIndexNext(3);*/
+var debug = true;
 
-    /*$('section[role="left-panel"] a').hover(function() {
-        $(this).find('i').addClass('fa-spin');
-    }, function() {
-        $(this).find('i').removeClass('fa-spin');
-    });*/
-})
+$(document).ready(function() {
+    if (!debug) {
+        $('body').fadeTo(1, 0.01);
+    }
+
+    $('body').removeClass('loading');
+
+    $('section[role="fullscreen"]').each(function() {
+        $(this).css('height', window.innerHeight + 'px');
+    });
+
+    if (!debug) {
+        setTimeout(function() {
+            $('body').fadeTo(1000, 1);
+        }, 1000);
+    }
+
+    $('nav ul li a').click(function() {
+        $.scrollTo($(this).attr('href'), 800);
+        history.replaceState({}, window.title, $(this).attr('href'));
+        return false;
+    });
+
+    $('#social button').click(function() {
+        window.open($(this).data('link'));
+    });
+});
